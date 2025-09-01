49e4ab1b57bc215c48964db25f59266a3c18be59
Daniel De Ruvo <deruvodaniel@gmail.com>
2025-09-01 16:52:52 -0300

    Updated index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
-import { RefsContext } from '../../context/refsContext';
-import { projects } from '../../arrays/arrayProjects';
+import { useRefs } from 'context/refsContext';
+import { projects } from 'arrays/arrayProjects';
 import {
   SectionProjects,
   ContainerProjects,
@@ -13,57 +13,61 @@ import {
   ProjectSubtitle,
   ProjectDescription,
   ContainerButtons,
-  ProjectButton,
-  TechStack,
-  TechTag
+  ProjectButton
 } from './projects.styles';
 
 export const Projects = () => {
-  const { refProjects } = useContext(RefsContext);
+  const { refProjects } = useRefs();
 
   return (
-    <SectionProjects ref={refProjects}>
-      <h2>Projects</h2>
-      <ContainerProjects>
-        {projects && projects.map((project, index) => (
-          <Container
-            key={index}
-            as={motion.div}
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6, delay: index * 0.1 }}
-            viewport={{ once: true }}
-          >
-            <ImageContainer>
-              <Img src={project.img} alt={project.title} />
-            </ImageContainer>
-            <ContainerTexts>
-              <ProjectTitle>{project.title}</ProjectTitle>
-              <ProjectSubtitle>{project.subtitle}</ProjectSubtitle>
-              <ProjectDescription>{project.text}</ProjectDescription>
-              <ContainerButtons>
-                <ProjectButton 
-                  href={project.link} 
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                  primary
-                >
-                  View Project
-                </ProjectButton>
-                {project.github && (
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+      viewport={{ once: true }}
+    >
+      <SectionProjects ref={refProjects}>
+        <h2>Projects</h2>
+        <ContainerProjects>
+          {projects && projects.map((project, index) => (
+            <Container
+              key={project.id}
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, delay: index * 0.1 }}
+              viewport={{ once: true }}
+            >
+              <ImageContainer>
+                <Img src={project.img} alt={project.title} />
+              </ImageContainer>
+              <ContainerTexts>
+                <ProjectTitle>{project.title}</ProjectTitle>
+                <ProjectSubtitle>{project.subtitle}</ProjectSubtitle>
+                <ProjectDescription>{project.text}</ProjectDescription>
+                <ContainerButtons>
                   <ProjectButton 
-                    href={project.github} 
+                    href={project.link} 
                     target="_blank" 
                     rel="noopener noreferrer"
+                    primary
                   >
-                    GitHub
+                    View Project
                   </ProjectButton>
-                )}
-              </ContainerButtons>
-            </ContainerTexts>
-          </Container>
-        ))}
-      </ContainerProjects>
-    </SectionProjects>
+                  {project.github && (
+                    <ProjectButton 
+                      href={project.github} 
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                    >
+                      GitHub
+                    </ProjectButton>
+                  )}
+                </ContainerButtons>
+              </ContainerTexts>
+            </Container>
+          ))}
+        </ContainerProjects>
+      </SectionProjects>
+    </motion.div>
   );
 };
