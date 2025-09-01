7747ca6d4c94d976baf5f74b66707dd8980e8ad1
Daniel De Ruvo <deruvodaniel@gmail.com>
2025-09-01 18:07:20 -0300

    Fix all imports and exports systematically

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import { RefsContextProvider } from './context/refsContext';
-import { Container, Main } from './app.styles';
-import Header from './components/Header';
-import Home from './components/Home/Index';
-import AboutMe from './components/AboutMe';
-import Technologies from './components/Technologies';
-import Projects from './components/Projects';
-import Courses from './components/Courses';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
-import SvgContainer from './components/SvgContainer/SvgContainer';
-import ModalLoading from './components/ModalLoading';
-import ModalMessageForm from './components/ModalMessageForm';
+import { ContainerApp, ContainerSections } from './app.styles';
+import { Header } from './components/Header';
+import { Home } from './components/Home/Index';
+import { AboutMe } from './components/AboutMe';
+import { Technologies } from './components/Technologies';
+import { Projects } from './components/Projects';
+import { Courses } from './components/Courses';
+import { Contact } from './components/Contact/Contact';
+import { Footer } from './components/Footer/Footer';
+import { SvgContainer } from './components/SvgContainer/SvgContainer';
 
 function App() {
   return (
     <RefsContextProvider>
-      <Container>
+      <ContainerApp>
         <Header />
-        <Main>
+        <ContainerSections>
           <Home />
           <AboutMe />
           <Technologies />
           <Projects />
           <Courses />
           <Contact />
-        </Main>
+        </ContainerSections>
         <Footer />
         <SvgContainer />
-        <ModalLoading />
-        <ModalMessageForm />
-      </Container>
+        <div id="modal-loading"></div>
+        <div id="modal-confirm"></div>
+      </ContainerApp>
     </RefsContextProvider>
   );
 }
