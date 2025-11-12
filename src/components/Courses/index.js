de5cb846d5714d80173a281ef65c7c3634f2c73d
Daniel De Ruvo <dderuvo@makingsense.com>
2025-11-12 15:31:03 -0300

    fix: Remove unnecessary computed property in Courses component

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -31,7 +31,7 @@ export const Courses = () => {
           <ContainerCourses>
             {courses.map(({ id, name, text, academy }) => {
               return (
-                <ContainerCourse key={id} style={{ ['--delay']: `${(id % 6) * 0.35}s` }}>
+                <ContainerCourse key={id} style={{ '--delay': `${(id % 6) * 0.35}s` }}>
                   <ContainerCourseName>
                     <div></div>
                     <CourseName>{name}</CourseName>
